c0623a8ea4938b062d8485768c5c73cc3ea68282
rifqi011 <rifqibanusafingi11@gmail.com>
2024-04-07 23:21:05 +0700

    Add admin

--- admin.php
+++ admin.php
@@ -1,11 +1,21 @@
+<!-- Include Koneksi Database dan Mulai Sesi -->
+<?php
+include 'koneksi.php';
+
+// Query Tabel setting dan Fetch Tabel
+$toko = mysqli_query($connect, "SELECT * FROM setting");
+$namatoko = mysqli_fetch_array($toko);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin - </title>
+    <title>Admin - <?php echo $namatoko['nama']; ?></title>
+    <link rel="stylesheet" href="admin/admincss/style.css">
 </head>
 <body>
-    
+    <h1>tes</h1>
 </body>
 </html>
